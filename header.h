1201ccd21143503bff753caba47004c56b2e1f8d
99002501 <71481755+99002501@users.noreply.github.com>
2020-09-19 12:56:21 +0530

    Update header.h

--- header.h
+++ header.h
@@ -3,3 +3,15 @@
 #include<stdlib.h>
 #include<string.h>
 char r;
+struct book{
+	char code[20];
+	char name[20];
+	char date[20];
+	int cost;
+}b;
+int seat=80;
+void insert_details();
+void view_All();
+void find();
+void book_ticket();
+void old_records();
